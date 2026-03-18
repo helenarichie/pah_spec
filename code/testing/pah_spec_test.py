06b709cad0f60cf241fa64be6b842497c7f594e3
helenarichie <helenarichie@gmail.com>
2026-03-17 22:28:45 -0400

    Revert "switch from unittests discover to pytest"
    This reverts commit 5895287b9fde7eda00dc96cb7eabdc27cba959df.

--- code/testing/pah_spec_test.py
+++ code/testing/pah_spec_test.py
@@ -1,6 +1,7 @@
 import io
 from math import isclose
 import os
+import sys
 import unittest
 import zipfile
 
@@ -10,6 +11,7 @@ import requests
 
 from scipy.integrate import trapezoid
 
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), "../"))
 import pah_spec
 
 # Set tolerance for disagreement to 1%
