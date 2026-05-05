d1fc92ba78ca0334c6baef9ae3ed3bc2524147b5
Matthew Abruzzo <matthewabruzzo@gmail.com>
2026-05-05 09:22:26 -0400

    track the version number within pah_spec

--- src/pah_spec/__init__.py
+++ src/pah_spec/__init__.py
@@ -1,4 +1,9 @@
 from .pah_spec import PahSpec, calc_pah_energy, GRAIN_SIZES
 from .pah_spec import __doc__
 
+# we consider __version__ to be a public name, but we don't list it the __all__
+# variable to avoid importing it when a user writes ``from pah_spec import *``
+# -> we import using `as` to silence the ruff warning that __version__ is unused
+from ._version import __version__ as __version__
+
 __all__ = ["PahSpec", "calc_pah_energy", "GRAIN_SIZES"]
