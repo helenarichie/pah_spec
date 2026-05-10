443f22620630143f080a73868d93084573178219
Matthew Abruzzo <matthewabruzzo@gmail.com>
2026-05-10 14:35:06 -0400

    rename pah_spec.py to _core, shift docstring to __init__.py and add cross-references

--- src/pah_spec/__init__.py
+++ src/pah_spec/__init__.py
@@ -1,5 +1,18 @@
-from .pah_spec import PahSpec, calc_pah_energy, GRAIN_SIZES
-from .pah_spec import __doc__
+"""
+pah_spec is a package for generating PAH emission spectra for arbitrary input radiation fields using the single photon
+approximation, as described in Richie & Hensley (2026).
+
+The primary functionality of pah_spec lies in the :py:class:`PahSpec` class, which uses a pre-existing set of basis spectra to
+execute the :py:func:`PahSpec.generate_spectrum` method. This method takes a radiation field and dust grain size distributions as
+(optional) inputs and returns an emission spectrum for neutral and ionized PAHs.
+
+In addition to generating emission spectra, pah_spec contains useful functions for the PAH energy and absorption
+cross-sections (:py:func:`calc_pah_energy` and :py:meth:`PahSpec.calc_c_abs`, respectively) using the PAH model from Draine & Li (2001)
+and Draine et al. (2021).
+"""
+
+
+from ._core import PahSpec, calc_pah_energy, GRAIN_SIZES
 
 # we consider __version__ to be a public name, but we don't list it the __all__
 # variable to avoid importing it when a user writes ``from pah_spec import *``
