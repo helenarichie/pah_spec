b4ff0ebec1d29207b7e239860cec7a73afe50011
Matthew Abruzzo <matthewabruzzo@gmail.com>
2026-05-10 14:37:04 -0400

    silence a spurious sphinx warning

--- docs/sphinx/conf.py
+++ docs/sphinx/conf.py
@@ -34,4 +34,4 @@ autodoc_default_options = {
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_static_path = ["_static"]
+#html_static_path = ["_static"]
