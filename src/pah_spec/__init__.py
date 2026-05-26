462c1f86440be4bf752ab80ee84251723aca7a91
helenarichie <helenarichie@gmail.com>
2026-05-26 11:47:43 -0400

    address #7 by removing grain sizes as module level variable and instead reading it from basis spectra data file

--- src/pah_spec/__init__.py
+++ src/pah_spec/__init__.py
@@ -11,12 +11,11 @@ cross-sections (:py:func:`calc_pah_energy` and :py:meth:`PahSpec.calc_c_abs`, re
 and Draine et al. (2021).
 """
 
-
-from ._core import PahSpec, calc_pah_energy, GRAIN_SIZES
+from ._core import PahSpec, calc_pah_energy
 
 # we consider __version__ to be a public name, but we don't list it the __all__
 # variable to avoid importing it when a user writes ``from pah_spec import *``
 # -> we import using `as` to silence the ruff warning that __version__ is unused
 from ._version import __version__ as __version__
 
-__all__ = ["PahSpec", "calc_pah_energy", "GRAIN_SIZES"]
+__all__ = ["PahSpec", "calc_pah_energy"]
